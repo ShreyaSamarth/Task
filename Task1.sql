63fb0a6512958df72f1f94313d9533e0e0f07884
ShreyaSamarth <72148564+ShreyaSamarth@users.noreply.github.com>
2021-05-21 00:27:35 +0530

    Create Task1.sql

---
+++ Task1.sql
@@ -0,0 +1,5 @@
+create table world.Emp_Table (id int(20), name varchar(20), salary int(20));
+
+insert into world.Emp_Table values (101, 'Ram', 20000), (102, 'Sham', 300000), (103, 'Sita', 5000);
+
+select * from world.Emp_Table;
